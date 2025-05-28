6a6c49fed2a5f4181a3360d5dafe163df144e33e
David Santana <davidsantana06@hotmail.com>
2025-05-28 15:49:30 -0300

    feat(human_vote): implement views

--- app/view/human_vote_view.py
+++ app/view/human_vote_view.py
@@ -10,36 +10,35 @@ from .mixin import ResponseMixin
 
 class HumanVoteView(FlaskView, ResponseMixin):
     def index(self):
-        # _, current_group = HumanVoteService.get_all()
-        # images = ImageService.get_all_by_group(current_group)
-        # is_voting = len(images) > 0
-        images = [
-            {"id": i + 1, "theme": "tema", "filename": "image-template.png"}
-            for i in range(3)
-        ]
-        is_voting = True
+        _, current_group = HumanVoteService.get_all()
+        images = ImageService.get_all_by_group(current_group)
+
+        is_voting = len(images) > 0
         if not is_voting:
             return self._redirect_to("image:index")
+
         return self._render_page("human_vote/index", images=images)
 
     def post(self):
-        # _, current_group = HumanVoteService.get_all()
-        # image = ImageService.get_one_by_group(current_group)
-        # is_voting = image is not None
-        image = {"id": 1, "theme": "tema", "filename": "image-template.png"}
-        is_voting = True
+        _, current_group = HumanVoteService.get_all()
+        image = ImageService.get_one_by_group(current_group)
+
+        is_voting = image is not None
         if not is_voting:
             Flash.append("success", "Você avaliou todas as imagens")
             return self._redirect_to("image:index")
+
         form = HumanVoteForm(request.form)
         HumanVoteService.create(form)
         return self._redirect_to("human_vote:index")
 
     def reset(self):
         next = request.args.get("next", "home:index")
-        evaluation = HumanVoteService.get_first()
-        has_voted_one = evaluation is not None
+        vote = HumanVoteService.get_first()
+
+        has_voted_one = vote is not None
         if has_voted_one:
             HumanVoteService.delete_all()
             Flash.append("info", "Seu progresso foi reiniciado")
+
         return self._redirect_to(next)
